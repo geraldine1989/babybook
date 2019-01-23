d213ccaedf7e836e8a51d7fa00c31a3fafe7f27f
Marie-K <marie.koual@gmail.com>
2019-01-23 18:55:29 +0100

    conflit resolved

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,28 +2,29 @@
  * Npm import
  */
 import React from 'react';
-
+import { NavLink, Route, Switch } from 'react-router-dom';
 /**
  * Local import
  */
 
-
-import Home from 'src/components/Home';
 import HeaderLogo from 'src/components/HeaderLogo';
-import MainNav from '../MainNav';
-import InscriptionForm from '../InscriptionForm';
-import LoginNanny from '../LoginNanny';
-import LoginParents from '../LoginParents';
-import Informations from '../Informations';
-import Footer from '../Footer';
-import LegalMentions from '../LegalMentions';
-import Contact from '../Contact';
-import ParentsNav from '../ParentsSpace/ParentsNav';
-import ParentsSpaceInfo from '../ParentsSpace/ParentsSpaceInfo';
-import Error from '../Error';
+import MainNav from 'src/components/MainNav';
+import Home from 'src/components/Home';
+import InscriptionForm from 'src/components/InscriptionForm';
+import LoginParents from 'src/components/LoginParents';
+import LoginNanny from 'src/components/LoginNanny';
+import MyDay from 'src/components/MyDay';
+import Informations from 'src/components/Informations';
+import ParentsNav from 'src/components/ParentsSpace/ParentsNav';
+import ParentsSpaceDayType from 'src/components/ParentsSpace/ParentsSpaceDayType';
+import ParentsSpaceInfo from 'src/components/ParentsSpace/ParentsSpaceInfo';
+import ParentsSpaceContacts from 'src/components/ParentsSpace/ParentsSpaceContacts';
+import LegalMentions from 'src/components/LegalMentions';
+import Contact from 'src/components/Contact';
+import NotFound from 'src/components/NotFound';
+import Footer from 'src/components/Footer';
 
 import './app.scss';
-
 
 /**
  * Code
@@ -31,12 +32,25 @@ import './app.scss';
 const App = () => (
   <div>
     <HeaderLogo />
-    <MainNav/>
-    <Error/>
-    <Footer/>
+    <MainNav />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/inscription" component={InscriptionForm} />
+      <Route exact path="/login-parents" component={LoginParents} />
+      <Route exact path="/login-nanny" component={LoginNanny} />
+      <Route exact path="/my-day/journal" component={MyDay} />
+      <Route exact path="/my-day/infos" component={Informations} />
+      <Route exact path="/espace-parents" component={ParentsNav} />
+      <Route exact path="/espace-parents/journee-type" component={ParentsSpaceDayType} />
+      <Route exact path="/espace-parents/infos" component={ParentsSpaceInfo} />
+      <Route exact path="/espace-parents/contacts" component={ParentsSpaceContacts} />
+      <Route exact path="/mentions-legales" component={LegalMentions} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/not-found" component={NotFound} />
+    </Switch>
 
+    <Footer />
   </div>
-
 );
 
 /**
