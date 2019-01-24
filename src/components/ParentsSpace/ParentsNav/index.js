61032f4a4a76dbc84d44bcbfd87a7df55e88d8ee
geraldine <geraldinesaul7@gmail.com>
2019-01-24 18:24:22 +0100

    responsive ok

--- src/components/ParentsSpace/ParentsNav/index.js
+++ src/components/ParentsSpace/ParentsNav/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { Input, Menu } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Input, Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 /**
  * Local import
@@ -8,12 +8,12 @@ import './style.scss';
 
 
 export default class ParentsNav extends Component {
-  state = { activeItem: 'Modifier les infos' }
+  state = { activeItem: 'Modifier journée type' }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
 
     return (
       <div id="parentsNav">
@@ -24,29 +24,31 @@ export default class ParentsNav extends Component {
           to="/espace-parents/journee-type"
           className="second-menu-item"
           name='Modifier journée type'
-          active={activeItem === 'Modifier journée type'}
+          //active={activeItem === 'Modifier journée type'}
           onClick={this.handleItemClick}
+          activeClasseName="active"
         />
         <Menu.Item
           as={NavLink}
           to="/espace-parents/infos"
           className="second-menu-item"
           name='Modifier les infos'
-          active={activeItem === 'Modifier les infos'}
+          //active={activeItem === 'Modifier les infos'}
           onClick={this.handleItemClick}
-        
+          activeClasseName="active"
         />
         <Menu.Item
           as={NavLink}
           to="/espace-parents/contacts"
           className="second-menu-item"
           name='Gestion contacts'
-          active={activeItem === 'Gestion contacts'}
+          //active={activeItem === 'Gestion contacts'}
           onClick={this.handleItemClick}
+          activeClasseName="active"
         />
       </Menu>
       </div>
 
-    )
+    );
   }
 }
