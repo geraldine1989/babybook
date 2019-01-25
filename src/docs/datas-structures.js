af6926dcc0605e2dfffb8ee32801ec4e73246874
Marc Delalande <spionit@gmail.com>
2019-01-25 10:27:28 +0100

    modif in datas structures

--- src/docs/datas-structures.js
+++ src/docs/datas-structures.js
@@ -1,20 +1,11 @@
-import { getMaxListeners } from "cluster";
 
-// users :
-let parents = {
+{
   id: "parents",
   parents: [
     {
-      id: "1",
       email:"toto@gmail.com",
       password: "password",
       passwordNanny: "otherPass",
-      firstName: "Toto",
-      lastName: "Nomdefamille",
-      mobileMom: "0612345678",
-      mobileDad: "0623456789",
-      phoneMom: "0123456789",
-      phoneDad: "0134567890",
       baby: [
         {
           firstName: "Tototounet",
@@ -24,22 +15,18 @@ let parents = {
             {
               medics: [
                 {
-                  name: "doliprane",
-                  qty: "1",
+                  name: "doliprane 1",
                 },
                 {
-                  name: "suppo",
-                  qty: "12",
+                  name: "suppo 12",
                 },
               ],
               vaccins: [
                 {
-                  name: "vaccin machin",
-                  date: "12/02/2018",
+                  name: "vaccin machin 12/02/2018",
                 },
                 {
-                  name: "vaccin bidule",
-                  date: "13/02/2018",
+                  name: "vaccin truc 12/02/2018",
                 },
               ],
               allergies: [
@@ -52,9 +39,19 @@ let parents = {
               ],
             }
           ],
+          phoneNumbers: [
+            {
+              name: "Maman",
+              phone: "0112345678",
+            },
+            {
+              name: "Papa",
+              phone: "0112345678",
+            },
+          ],
           myDay: [
             {
-              date: Date(),
+              date: "01/02/2000",
               note: "Ceci est une note des parents pour la journée.",
               itemList: [
                 {
@@ -76,105 +73,17 @@ let parents = {
           contacts: [
             {
               firstName: "Mamie",
-              lastName: "",
-              phone: "0123456789",
+              email: "mamie@me.com",
             },
             {
-              firstName: "Nanny",
-              lastName: "Dupont",
-              phone:"0612345678",
+              firstName: "Tonton",
+              email: "tonton@me.com",
             },
           ],
         },
       ],
     },
-    {
-      id: "2",
-      email:"tata@gmail.com",
-      password: "password",
-      passwordNanny: "otherPass",
-      firstName: "Tata",
-      lastName: "Ratata",
-      mobileMom: "0612345678",
-      mobileDad: "0623456789",
-      phoneMom: "0123456789",
-      phoneDad: "0134567890",
-      baby: [
-        {
-          firstName: "Victor",
-          lastName: "Hugo",
-          birthDate: "26-02-1802",
-          health: [
-            {
-              medics: [
-                {
-                  name: "doliprane",
-                  qty: "1",
-                },
-                {
-                  name: "suppo",
-                  qty: "12",
-                },
-              ],
-              vaccins: [
-                {
-                  name: "vaccin machin",
-                  date: "12/02/2018",
-                },
-                {
-                  name: "vaccin bidule",
-                  date: "13/02/2018",
-                },
-              ],
-              allergies: [
-                {
-                  name: "cacahuètes",
-                },
-                {
-                  name: "chat",
-                },
-              ],
-            }
-          ],
-          myDay: [
-            {
-              date: Date(),
-              note: "Ceci est une note des parents pour la journée.",
-              itemList: [
-                {
-                  name: "Biberon",
-                  hour: "08:00",
-                  indic: "Mettre 3 dosettes avec 250 cl de lait",
-                  note: "Tototounet a tout bu !!!",
-                },
-                {
-                  name: "Sieste",
-                  hour: "10:00",
-                  indic: "Dort avec son doudou girafe",
-                  note: "Tototounet a dormi 3h !",
-                },
-              ],
-              nannyNote: "Très bonne journée. Peinture sur les murs. S'est fait les dents sur un pied de table."
-            },
-          ],
-          contacts: [
-            {
-              firstName: "Mamie",
-              lastName: "Hugo",
-              phone: "0123456789",
-            },
-            {
-              firstName: "Jean",
-              lastName: "Valjean",
-              phone:"0612345678",
-            },
-          ],
-        },
-      ],
-    },
-  ],
-  
-}
+   
 
 // emails inscrits :
 let emails = [
