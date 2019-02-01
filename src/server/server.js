ebc0508596ebb9917c08ad9823b15d62d077efa1
Lutheen <jnicolleau@gmail.com>
2019-02-01 18:13:58 +0100

    Server linter problems fixed

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var app = express();
-var port = 3000;
-var bodyParser = require('body-parser');
+const express = require('express');
 
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -14,52 +14,53 @@ app.use(function(req, res, next) {
   next();
 });
 
-var mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/babybook", { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/babybook', { useNewUrlParser: true });
 
 /**
 * var
 */
-var registeredEmails = new mongoose.Schema({
- email: String,
- password: String,
- accessCode: String
+const registeredEmails = new mongoose.Schema({
+  email: String,
+  password: String,
+  accessCode: String,
 });
-var registered_emails = mongoose.model("registered_emails", registeredEmails);
+const registered_emails = mongoose.model('registered_emails', registeredEmails);
 
-var registeredChildren = new mongoose.Schema({
+const registeredChildren = new mongoose.Schema({
   firstname: String,
   lastname: String,
-  birthdate: String
- });
- var registered_children = mongoose.model("registered_children", registeredChildren);
+  birthdate: String,
+});
+const registered_children = mongoose.model('registered_children', registeredChildren);
 /**
 * les routes
 */
 /**
 * Récupération de la liste de emails enregistrés
 */
-var regEmails = [];
-app.get("/getEmails", (req, res) => {
- function findEmails() {
-   return new Promise(function(resolve, reject) {
+let regEmails = [];
+app.get('/getEmails', (req, res) => {
+  function findEmails() {
+    return new Promise(((resolve, reject) => {
 
-     registered_emails.find(function (err, response) {
-       regEmails = response;
-       resolve (response);
-       return response;
-     })
-   })
- }
- findEmails()
- .then(function(response) {
-   regEmails = response;
-   regEmails = regEmails.map(email => email.email);
- })
- .catch(function(err) {
-   console.log('Caught an error!', err);
- });
+      registered_emails.find((err, response) => {
+        regEmails = response;
+        resolve (response);
+        return response;
+      });
+    }));
+  }
+  findEmails()
+    .then((response) => {
+      regEmails = response;
+      regEmails = regEmails.map(email => email.email);
+    })
+    .catch((err) => {
+      console.log('Caught an error!', err);
+    });
 });
 
 /**
@@ -67,51 +68,49 @@ app.get("/getEmails", (req, res) => {
 */
 console.log(regEmails);
 
-app.post("/inscription", (req, res) => {
- var newUser = new registered_emails(req.body);
- console.log(regEmails);
- console.log('newUserEmail : ' + newUser.email);
- const emailExist = regEmails.filter(email => newUser.email === email)
- console.log(emailExist);
-   if (emailExist[0]) {
-     res.send('notOk');
-   } else {
-
-     newUser.save()
-     .then(item => {
-       res.send("Name saved to database");
-     })
-     .catch(err => {
-       res.status(400).send("Unable to save to database");
-     });
-   }
-
-});
-
-app.post("/postchild", (req, res) => {
-  var newUser = new registered_children(req.body);
+app.post('/inscription', (req, res) => {
+  const newUser = new registered_emails(req.body);
   console.log(regEmails);
-  console.log('newUserEmail : ' + newUser.email);
+  console.log(`newUserEmail : ${newUser.email}`);
   const emailExist = regEmails.filter(email => newUser.email === email);
   console.log(emailExist);
-    if (emailExist[0]) {
-      res.send('notOk');
-    } else {
- 
-      newUser.save()
-      .then(item => {
-        res.send("Name saved to database");
+  if (emailExist[0]) {
+    res.send('notOk');
+  }
+  else {
+    newUser.save()
+      .then((item) => {
+        res.send('Name saved to database');
       })
-      .catch(err => {
-        res.status(400).send("Unable to save to database");
+      .catch((err) => {
+        res.status(400).send('Unable to save to database');
       });
-    }
- 
- });
+  }
+});
+
+app.post('/postchild', (req, res) => {
+  const newUser = new registered_children(req.body);
+  console.log(regEmails);
+  console.log(`newUserEmail : ${newUser.email}`);
+  const emailExist = regEmails.filter(email => newUser.email === email);
+  console.log(emailExist);
+  if (emailExist[0]) {
+    res.send('notOk');
+  }
+  else {
+    newUser.save()
+      .then((item) => {
+        res.send('Name saved to database');
+      })
+      .catch((err) => {
+        res.status(400).send('Unable to save to database');
+      });
+  }
+});
 
 /**
 * Listen PORT 3000
 */
 app.listen(port, () => {
-   console.log("Server listening on port " + port);
+  console.log('Server listening on port ' + port);
 });
