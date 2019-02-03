7249f0314effa4d986b3caf23d5dff861e2056cf
Marc Delalande <spionit@gmail.com>
2019-02-03 11:36:54 +0100

    sending email from server ok

--- src/server/server2.js
+++ src/server/server2.js
@@ -110,3 +110,46 @@ app.post("/loginParents", (req, res) => {
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
+
+"use strict";
+const nodemailer = require("nodemailer");
+
+// async..await is not allowed in global scope, must use a wrapper
+async function sendEmail(){
+
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  // let account = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: 'spionit@gmail.com', // generated ethereal user
+      pass: 'ttrchecpoihufqcs' // generated ethereal password
+    }
+  });
+
+  // setup email data with unicode symbols
+  const mailOptions = {
+    from: '"Babybook" <spionit@gmail.com>', // sender address
+    to: "spionit@gmail.com", // list of receivers
+    subject: "Hello", // Subject line
+    text: "Merci pour votre inscription à Babybook", // plain text body
+    html: "<b>Hello world?</b>" // html body
+  };
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail(mailOptions)
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+sendEmail().catch(console.error);
